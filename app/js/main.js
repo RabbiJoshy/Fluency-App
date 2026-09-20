58734dbfe37b9b3330419b8a8d73532f0d626df4
Joshua Thomas Amar <joshuasthomas@me.com>
2026-09-20 16:59:15 +0100

    Deploy source-list frequencies and desktop card panels

--- app/js/main.js
+++ app/js/main.js
@@ -12,7 +12,7 @@ import './config.js?v=20260919c';
 import './progress.js?v=20260920e';
 import './knowledge.js?v=20260920a';
 import './ui.js?v=20260920f';
-import './vocab.js?v=20260920e';
+import './vocab.js?v=20260920f';
 import './cognates.js?v=20260914e';
 import './coverage.js?v=20260909a';
 import './fast-mode.js?v=20260916a';
@@ -21,7 +21,7 @@ import './song-sets.js?v=20260823ae';
 import './playlist-live.js?v=20260919a';
 import './spotify-playlist-import.js?v=20260919a';
 import './vocabulary-import.js?v=20260913a';
-import './flashcards.js?v=20260920d';
+import './flashcards.js?v=20260920e';
 import { validateArtistCatalog } from './data-contracts.js?v=20260825ak';
 
 function startCardTutorial() {
